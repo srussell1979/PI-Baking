adf66cc3483532a7f60ee9da949b664bd52b79eb
Scott C Russell <59456852+srussell1979@users.noreply.github.com>
2023-09-09 22:15:52 -0500

    Add files via upload

---
+++ RM4000.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+echo dtparam=i2c_arm=on,i2c_arm_baudrate=400000 >> /boot/config.txt
+echo dtoverlay=gpio-shutdown,gpio_pin=4,active_low=1,gpio_pull=up >> /boot/config.txt
+echo " "
+echo " "
+echo " "
+# Clone SKU_RM0004 library
+echo "----------------------------------------------------------------"
+echo "Commence SKU_RM0004 Library
+git clone https://github.com/UCTRONICS/SKU_RM0004.git"
+echo "----------------------------------------------------------------"
+echo " "
+echo " "
+echo " "
+echo "----------------------------------------------------------------"
+# Compile
+cd SKU_RM0004
+make
+echo "----------------------------------------------------------------"
+echo " "
+echo " "
+echo " "
+echo "----------------------------------------------------------------"
+# Run
+./display
+echo "----------------------------------------------------------------"
+echo " "
+echo " "
+echo " "
+echo "----------------------------------------------------------------"
+# Add Automatic Start Script
+echo cd /home/pi/SKU_RM0004
+echo make clean 
+echo make 
+echo ./display &
+echo "----------------------------------------------------------------"
+echo " "
+echo " "
+echo " "
+echo "----------------------------------------------------------------"
+sudo reboot
